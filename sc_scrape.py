968b31b49087514778d5cd243ae56e78ac2e10f6
agk2000 <anish.karpurapu@duke.edu>
2022-03-13 15:27:07 -0500

    adding Fearless TV album

--- sc_scrape.py
+++ sc_scrape.py
@@ -1,3 +1,5 @@
+import json
+
 lines = []
 with open('/Users/anish/Documents/anish/wordle/red.txt') as f:
     lines = f.readlines()
@@ -10,11 +12,12 @@ for line in lines:
         count += 1
         line = line.replace("\"", "@")
         arr = line.split(" ")
+        #new_url_suffix = arr[3].split("?")[0].split("@")[1]
         new_url_suffix = arr[5].split("?")[0].split("@")[1]
         new_url = "https://soundcloud.com" + new_url_suffix
         new_title = "Taylor Swift - " + line.split("@>")[1].split("<")[0]
         #print(f'line {count}: {new_url}; {new_title}')   
         title_list.append(new_title)
         url_list.append(new_url)
-print(url_list)
-print(title_list)
+print(json.dumps(url_list, ensure_ascii=False))
+print(json.dumps(title_list, ensure_ascii=False))
